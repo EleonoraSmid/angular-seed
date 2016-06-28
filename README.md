ec2eb036631bd83b571137a969846054a9941d0e
EleonoraSmid <eleonora.smid@gmail.com>
2016-06-28 12:27:46 +0200

    Gulp explenation

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ An Angular 1.5 skeleton
 npm install
 
 node server.js
+
+Gulp to compile your less files 
+
+Gulp build to minify your CSS files
--- server.js
+++ server.js
@@ -6,5 +6,6 @@ app.use('/js', express.static(__dirname + '/src'))
 app.use('/angular', express.static(__dirname + '/node_modules/angular'))
 app.use('/angular-route', express.static(__dirname + '/node_modules/angular-route'))
 app.listen(3000, function(res,req){
-
+	console.log('Server running on port 3000!')
+	console.log('Use "Gulp" to compile less and "Gulp build" to minify your CSS')
 })
